c3cac820e42851133a80ff193561f76ffeb09898
maciej565 <maciej565@gmail.com>
2022-02-17 22:42:35 +0100

    Added main Controller, main Model, main View and Error handler

---
+++ Core/View.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core;
+
+class View
+{
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+        $file = dirname(__DIR__) . "/App/Views/$view";
+        if (is_readable($file)) 
+        {
+            require $file;
+        } 
+        else 
+        {
+            throw new \Exception("$file not found");
+        }
+    }
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) 
+        {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $twig = new \Twig\Environment($loader);
+            $twig->addGlobal('current_user', \App\Auth::getUser());
+            
+        }
+        echo $twig->render($template, $args);
+    }
+    public static function getTemplate($template, $args = [])
+    {
+        static $twig = null;
+        
+        if ($twig === null) 
+        {
+            $loader = new \Twig\Loader\FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $twig = new \Twig\Environment($loader);
+            $twig->addGlobal('current_user', \App\Auth::getUser());
+        }
+        return $twig->render($template, $args);
+    }
+}
